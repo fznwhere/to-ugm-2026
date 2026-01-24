42143cd822aed0b1e202a6546597b1c24f54f6be
fznwhere <fauzanwar7@gmail.com>
2026-01-24 18:24:04 +0700

    Update versi sw-final.js

--- sw-final.js
+++ sw-final.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'to-ugm-v10'; // Versi dinaikkan agar cache diperbarui
+const CACHE_NAME = 'to-ugm-v10.1.1.1.1.1.1'; // Versi dinaikkan agar cache diperbarui
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
